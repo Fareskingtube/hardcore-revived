d8f7b4160b26fbfd81afacfd530f6f4ace570eeb
Fareskingtube <83561978+Fareskingtube@users.noreply.github.com>
2026-06-14 16:01:11 +0300

    Initial commit (Let's do this)

---
+++ src/client/java/net/fareskingtube/client/HardcoreRevivedClient.java
@@ -0,0 +1,10 @@
+package net.fareskingtube.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardcoreRevivedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/fareskingtube/client/HardcoreRevivedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fareskingtube.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HardcoreRevivedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
