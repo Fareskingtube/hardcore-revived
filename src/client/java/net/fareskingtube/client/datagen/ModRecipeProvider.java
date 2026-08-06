52a7a86c7185531ca2008f2dcc12fb295f24be18
Fareskingtube <83561978+Fareskingtube@users.noreply.github.com>
2026-08-06 10:36:36 +0300

    Add improvements to resuscitatio_mortuorum book and recipes to most items

--- src/client/java/net/fareskingtube/client/datagen/ModRecipeProvider.java
+++ src/client/java/net/fareskingtube/client/datagen/ModRecipeProvider.java
@@ -2,12 +2,15 @@ package net.fareskingtube.client.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fareskingtube.block.ModBlocks;
 import net.fareskingtube.component.ModDataComponentTypes;
 import net.fareskingtube.item.ModItems;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
@@ -20,6 +23,33 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter exporter) {
-
+        // Revival Altar Recipe
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.REVIVAL_ALTAR)
+                .pattern("BGB")
+                .pattern("BDB")
+                .pattern("DDD")
+                .input('B', ModBlocks.BLOOD_BLOCK)
+                .input('G', Blocks.GOLD_BLOCK)
+                .input('D', Blocks.DEEPSLATE_TILES)
+                .criterion(hasItem(ModBlocks.REVIVAL_ALTAR), conditionsFromItem(ModBlocks.BLOOD_BLOCK))
+                .offerTo(exporter);
+        // Heart Injector recipe (temporary)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.HEART_INJECTOR)
+                .pattern("I ")
+                .pattern(" G")
+                .input('I', Items.IRON_INGOT)
+                .input('G', Items.GLASS_BOTTLE)
+                .criterion(hasItem(Items.GLASS_BOTTLE), conditionsFromItem(ModItems.HEART_INJECTOR))
+                .offerTo(exporter);
+        // Heart Extractor recipe (temporary)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.HEART_EXTRACTOR)
+                .pattern("S S")
+                .pattern("SSS")
+                .pattern(" S ")
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(ModItems.HEART_EXTRACTOR))
+                .offerTo(exporter);
+        //  TODO: Change this recipe to an item that drops blood from damaging mobs or from extracting your own blood
+        offer2x2CompactingRecipe(exporter, RecipeCategory.MISC, ModBlocks.BLOOD_BLOCK, Blocks.REDSTONE_BLOCK);
     }
 }
