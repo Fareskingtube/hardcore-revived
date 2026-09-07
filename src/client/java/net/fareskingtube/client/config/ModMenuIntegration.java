6a35ac29fbb2dec666f655d28990e0ff1bf51058
Fareskingtube <firoobasha@gmail.com>
2026-09-07 17:21:05 +0300

    Add butcher_knife and blood item with README Fixes

---
+++ src/client/java/net/fareskingtube/client/config/ModMenuIntegration.java
@@ -0,0 +1,35 @@
+package net.fareskingtube.client.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.isxander.yacl3.api.*;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
+import net.minecraft.text.Text;
+
+// In testing will be reworked
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parentScreen -> YetAnotherConfigLib.createBuilder()
+                .title(Text.literal("Used for narration. Could be used to render a title in the future."))
+                .category(ConfigCategory.createBuilder()
+                        .name(Text.literal("Name of the category"))
+                        .tooltip(Text.literal("This text will appear as a tooltip when you hover or focus the button with Tab. There is no need to add \n to wrap as YACL will do it for you."))
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.literal("Name of the group"))
+                                .description(OptionDescription.of(Text.literal("This text will appear when you hover over the name or focus on the collapse button with Tab.")))
+                                .option(Option.<Boolean>createBuilder()
+                                        .name(Text.literal("Boolean Option"))
+                                        .description(OptionDescription.of(Text.literal("This text will appear as a tooltip when you hover over the option.")))
+                                        .binding(true, () -> {
+                                            return true;
+                                        }, newVal -> {
+                                        })
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+                .generateScreen(parentScreen);
+    }
+}
